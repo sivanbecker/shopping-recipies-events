995d809cf2263b15f19f5919ae50bd5bcfca5f65
Sivan Becker <sbecker@infinidat.com>
2026-04-09 10:44:25 +0300

    fix: remove ReactQueryDevtools floating button

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from 'sonner'
 
 import App from './App'
@@ -20,7 +19,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <Toaster richColors position="top-center" />
         </AuthProvider>
       </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 )
